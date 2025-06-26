9aefdfd042baca4adf2b64505eb7c9682b733ca3
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-06-25 23:03:44 -0300

    feat: add sequelize orm to interact with postgres database

--- b-and-f/backend/util/db.js
+++ b-and-f/backend/util/db.js
@@ -1,19 +1,20 @@
-const { Sequelize } = require('sequelize');
-const sqlFormatter = require('sql-formatter');
+const { Sequelize } = require("sequelize");
+const sqlFormatter = require("sql-formatter");
 
 const sequelize = new Sequelize(
   process.env.POSTGRES_DB,
   process.env.POSTGRES_USER,
   process.env.POSTGRES_PASSWORD,
   {
-    dialect: 'postgres',
-    host: 'postgres',
+    dialect: "postgres",
+    host: "postgres",
     logging: (msg) => {
-      const formattedSql = sqlFormatter.format(msg, { language: 'postgresql' });
+      const formattedSql = sqlFormatter.format(msg, { language: "postgresql" });
       console.log(formattedSql);
+      console.log("+=======================================================+");
     },
-    logQueryParameters: true
+    logQueryParameters: true,
   }
 );
 
-module.exports = sequelize;  
+module.exports = sequelize;
