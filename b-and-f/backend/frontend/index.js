aa655fc582aba63584e0782fd4a6ebc4aa5329e4
Lucas Almeida <lucas-lxx@alu.ufc.br>
2025-06-25 23:02:31 -0300

    feat: add functionality with javascript and skeleton of page of html

--- b-and-f/backend/frontend/index.js
+++ b-and-f/backend/frontend/index.js
@@ -1,18 +1,19 @@
 const table = document.getElementById("table");
 const refresh_btn = document.getElementById("refresh-btn");
 const submit_btn = document.getElementById("submit-btn");
+const update_btn = document.getElementById("update-btn");
 let table_data = [];
 
 async function createElementTdWithValAndClass(val, cls) {
-  const td = document.createElement('td');
+  const td = document.createElement("td");
   td.classList.add(cls);
   td.innerText = val;
   return td;
 }
 
 async function createBtnWithValAndClass(val, cls) {
-  const td = document.createElement('td');
-  const btn = document.createElement('button');
+  const td = document.createElement("td");
+  const btn = document.createElement("button");
   td.classList.add(cls);
   btn.innerText = val;
   td.appendChild(btn);
@@ -25,41 +26,60 @@ async function updateTdByClassFrom(from, val, cls) {
 }
 
 async function updateTable(data) {
-  for(idx in data) {
+  for (idx in data) {
     const user = data[idx];
     let tr = document.getElementById(user.id);
     if (!tr) {
-      tr = document.createElement('tr');
-      tr.setAttribute('id', user.id);
+      tr = document.createElement("tr");
+      tr.setAttribute("id", user.id);
 
-      const name = await createElementTdWithValAndClass(user.name, 'name');
-      const course = await createElementTdWithValAndClass(user.course, 'course');
-      const ira = await createElementTdWithValAndClass(user.ira, 'ira');
-      const del = await createBtnWithValAndClass('del', 'del-btn')
+      const name = await createElementTdWithValAndClass(user.name, "name");
+      const course = await createElementTdWithValAndClass(
+        user.course,
+        "course"
+      );
+      const ira = await createElementTdWithValAndClass(user.ira, "ira");
+      const del = await createBtnWithValAndClass("del", "del-btn");
+      const edt = await createBtnWithValAndClass("edt", "edt-btn");
 
       table.appendChild(tr);
       tr.appendChild(name);
       tr.appendChild(course);
       tr.appendChild(ira);
       tr.appendChild(del);
+      tr.appendChild(edt);
 
-      del.addEventListener('click', async (event) => {
+      // remove row from frontend and database
+      del.addEventListener("click", async (event) => {
         axios.delete(`/user/${user.id}`);
         table.removeChild(tr);
-      })
+      });
+
+      edt.addEventListener("click", async (event) => {
+        document.getElementById("myModal").style.display = "flex";
+        const id = document.getElementById("update-id");
+        const name = document.getElementById("update-name");
+        const course = document.getElementById("update-course");
+        const ira = document.getElementById("update-ira");
+        id.value = user.id;
+        name.value = user.name;
+        course.value = user.course;
+        ira.value = user.ira;
+        // table.appendChild(form);
+      });
     } else {
       await Promise.all([
-        updateTdByClassFrom(tr, user.name, 'name'),
-        updateTdByClassFrom(tr, user.course, 'course'),
-        updateTdByClassFrom(tr, user.ira, 'ira')
+        updateTdByClassFrom(tr, user.name, "name"),
+        updateTdByClassFrom(tr, user.course, "course"),
+        updateTdByClassFrom(tr, user.ira, "ira"),
       ]);
     }
   }
 }
 
 async function treatRefreshButton() {
-  console.log('HIT');
-  const res = await axios.get('/user')
+  console.log("HIT");
+  const res = await axios.get("/user");
   const data = res.data;
   table_data = data;
   updateTable(data);
@@ -67,22 +87,47 @@ async function treatRefreshButton() {
 
 async function createAlumni(event) {
   event.preventDefault();
-  const name = document.getElementById('name').value;
-  const course = document.getElementById('course').value;
-  const ira = document.getElementById('ira').value;
-  const res = await axios.post('/user', {
-    name,
-    course,
-    ira
+  const name = document.getElementById("name");
+  const course = document.getElementById("course");
+  const ira = document.getElementById("ira");
+  const res = await axios.post("/user", {
+    name: name.value,
+    course: course.value,
+    ira: ira.value,
   });
-
+  name.value = "";
+  course.value = "";
+  ira.value = "";
   console.log(res);
+  name.focus();
+  await treatRefreshButton();
 }
 
-const refresh_interval_id = setInterval(treatRefreshButton, 5000);
+async function updateAlumni() {
+  const id = document.getElementById("update-id");
+  const name = document.getElementById("update-name");
+  const course = document.getElementById("update-course");
+  const ira = document.getElementById("update-ira");
+  const data = {};
 
-document.addEventListener('load', treatRefreshButton)
-refresh_btn.addEventListener('click', treatRefreshButton);
-table.addEventListener('mouseenter', treatRefreshButton)
+  if (name.value !== "") data.name = name.value;
+  if (course.value !== "") data.course = course.value;
+  if (ira.value !== "") data.ira = ira.value;
 
-document.addEventListener('submit', createAlumni);
+  const res = await axios.patch(`/user/${id.value}`, data);
+  id.value = "";
+  name.value = "";
+  course.value = "";
+  ira.value = "";
+  console.log(res);
+
+  document.getElementById("myModal").style.display = "none";
+  await treatRefreshButton();
+}
+
+refresh_btn.addEventListener("click", treatRefreshButton);
+table.addEventListener("mouseenter", treatRefreshButton);
+submit_btn.addEventListener("click", createAlumni);
+update_btn.addEventListener("click", updateAlumni);
+
+treatRefreshButton();
